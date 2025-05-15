2590c8ca8a462a823c0c1521d56b4b9a3012398b
auuffaa <khiayeette@gmail.com>
2025-05-16 06:28:04 +0800

    Add files via upload

---
+++ tugas12.php
@@ -0,0 +1,8 @@
+<?php
+$i=1;
+$belanja =["aerostreet","compass","ortus","specs"];
+ echo "daftar koleksi </br>";
+ foreach ($belanja as $DaftarKoleksi) {
+    echo $i++ . "." $DaftarKoleksi . "</br>";
+}
+?>
